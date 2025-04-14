edc7b96fb17a721c26c437a3cbf45873796963e3
Naomi Green <naomipgreen@gmail.com>
2025-04-14 23:59:29 +0100

    add error handling

--- server.js
+++ server.js
@@ -19,8 +19,12 @@ const port = process.env.PORT || 4040;
 if (process.env.NODE_ENV === 'production') {
   // app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  app.get('/*', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'), (err)=>{
+      if(err){
+        res.status(500).send(err)
+      }
+    });
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
