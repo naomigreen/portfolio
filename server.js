212ffae0d04c3fdc9ecd93c316e3b16cca3e718a
Naomi Green <naomipgreen@gmail.com>
2025-04-14 22:35:22 +0100

    config testing

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors')
 const enforce = require('express-sslify');
-
 const stockData =require('./data/stockData.json')
 const gdpData = require('./data/gdpData.json')
 const gameData = require('./data/gamingData.json')
+
+const app = express();
+app.use(cors())
 
 app.get('/api/data/chartData', (req, res) => {
   res.send({stockData, gdpData, gameData});
 });
 
 const port = process.env.PORT || 4040;
-const app = express();
-app.use(cors())
+
 
 if (process.env.NODE_ENV === 'production') {
   // app.use(enforce.HTTPS({ trustProtoHeader: true }));
