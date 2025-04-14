1156ef515d1c26cc093f776e7ad686f184448956
Naomi Green <naomipgreen@gmail.com>
2025-04-15 00:10:45 +0100

    update

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ const port = process.env.PORT || 4040;
 if (process.env.NODE_ENV === 'production') {
   // app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
   // app.get('/*', (req, res)=> {
   //   res.sendFile(path.join(__dirname, 'client/build/index.html'), (err)=>{
   //     if(err){
