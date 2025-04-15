f4bc08aee6b45d556e190f4a3c8a87fcfc552a59
Naomi Green <naomipgreen@gmail.com>
2025-04-15 01:23:46 +0100

    update

--- server.js
+++ server.js
@@ -11,15 +11,16 @@ const port = process.env.PORT || 4040;
 
 app.use(cors())
 
-if (process.env.NODE_ENV === 'production') {
+app.get('/api/data/chartData', (req, res) => {
+  res.send({stockData, gdpData, gameData});
+});
+
+if (process.env.NODE_ENV === 'production') {  
   app.use(express.static('client/build'));
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
 
-app.get('/api/data/chartData', (req, res) => {
-  res.send({stockData, gdpData, gameData});
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
