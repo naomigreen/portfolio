b1089e2a23acfce59b84d2b86e73093457ce4345
Naomi Green <naomipgreen@gmail.com>
2025-04-15 01:52:56 +0100

    update

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.get('/api/data/chartData', (req, res) => {
   res.send({stockData, gdpData, gameData});
 });
 
-if (process.env.NODE_ENV === 'production') {  
-  app.use('/', express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
