95fdf2e9e55681d01e27c56530db335e5fc15ac4
Naomi Green <naomipgreen@gmail.com>
2025-04-15 01:06:49 +0100

    update

--- server.js
+++ server.js
@@ -2,27 +2,24 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors')
 const enforce = require('express-sslify');
-
+const app = express();
 const stockData =require('./data/stockData.json')
 const gdpData = require('./data/gdpData.json')
 const gameData = require('./data/gamingData.json')
 
 const port = process.env.PORT || 4040;
-const app = express();
 
 app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.get('/api/data/chartData', (req, res) => {
   res.send({stockData, gdpData, gameData});
 });
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}else{
-  app.get('/', (req, res) => res.send('server'))
-}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
