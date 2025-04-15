2a80ed2903b3aabaeb5b7ef66d9c2f2a30b5a0fb
Naomi Green <naomipgreen@gmail.com>
2025-04-15 01:43:47 +0100

    update

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get('/api/data/chartData', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {  
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
+  app.use('/', express.static('client/build'));
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
