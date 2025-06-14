8841b0e573a1cb7913f18064c4ec12489352b04b
Naomi Green <naomipgreen@gmail.com>
2025-06-14 19:35:37 +0100

    add test page

---
+++ client/src/containers/pages/Test.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Test = () => (
+  <Container>
+    <span>Test page</span>
+  </Container>
+)
+
+const Container = styled.div`
+  display: block;
+`
+
+export default Test
